50c166f9f2b63bda4e714251e1cb9d702846cf40
Suhail Kakar <suhail.zahed@gmail.com>
2022-12-13 00:34:24 +0530

    Update index.js

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -15,6 +15,7 @@ export default function Home() {
   }, []);
 
   useEffect(() => {
+    console.log(process.env.REACT_APP_MUX_ENV_KEY, "mux env key");
     if (playerRef) {
       const initTime = mux.utils.now();
 
